6d10a917956161a207e38c5ef5f2433ea1f5ad8a
Rafael Bressan <rfbressan@gmail.com>
2023-08-21 19:35:55 -0300

    fix(solow): composing Solow class with Production

--- solow/main.py
+++ solow/main.py
@@ -1,27 +1,94 @@
 # Script simulating a Solow-Swan growth model
 
+import matplotlib.pyplot as plt
 import numpy as np
-from solow import Solow
 
-cd_parameters = {"s": 0.3, "delta": 0.4, "k0": 0.25, "production": "Cobb-Douglas", "A": 2.0, "alpha": 0.3}
+from solow import CES, CobbDouglas, Solow
 
-# Instantiate the model
-cd_model = Solow(**cd_parameters)
+# Auxiliary functions definitions
+
+
+def plot45(model, kgrid):
+    kstar = model.steady_state()
+    fig, ax = plt.subplots(figsize=(8, 6))
+    ax.plot(kgrid, model.k_update(kgrid), "b-", label="$k_{t+1}$")
+    ax.plot(kgrid, kgrid, "k--", label="$45$")
+    # plot a star at the steady state
+    ax.plot(kstar, kstar, "*", markersize=10.0, color="green")
+    # annotate the value of kstar
+    ax.annotate(
+        f"$k^*=${kstar:.2f}",
+        xy=(kstar, kstar),
+        xytext=(kstar + 0.1, kstar - 0.1),
+        fontsize=12,
+        color="green",
+    )
+    ax.set_title("Capital accumulation")
+    ax.set_xlabel("$k_t$")
+    ax.set_ylabel("$k_{t+1}$")
+    ax.legend()
+    plt.show()
+
+
+def plot_consumption_savings(model, sgrid):
+    cons_dict = model.consumption_savings(sgrid)
+    consumpiton = cons_dict["consumption"]
+    savings_gr = cons_dict["savings_gr"]
+    consumpiton_gr = cons_dict["consumption_gr"]
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.plot(sgrid, consumpiton, "b-", label="Consumption")
+    ax.plot(savings_gr, consumpiton_gr, "g*", label="Golden Rule")
+    ax.set_xlabel("Savings rate")
+    ax.set_ylabel("Consumption per capita")
+    ax.legend()
+    plt.show()
+
+
+def plot_time_series(model, time_series):
+    # Plot time_series
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.plot(time_series, "b-", label="$Capital$")
+    # Horizontal dashed line at steady state
+    ax.hlines(model.steady_state(), 0, 11, "red", "dashed")
+    ax.set_xlabel("Period")
+    ax.set_ylabel("Capital per capita")
+    plt.show()
+
+
+cd_parameters = {
+    "A": 2.0,
+    "alpha": 0.3,
+}
+# Create a cobbdouglas production function
+cobb_douglas = CobbDouglas(**cd_parameters)
+# Create the Solow model with CD production function
+cd_model = Solow(savings=0.3, delta=0.4, k_0=0.25, production=cobb_douglas)
 # print(model)
-# Plot the 45-degree line and the capital accumulation curve
-# cd_model.plot45(np.linspace(0, 3, 1000), cd_model.steady_state())
-# Plot the evolution of capital per capita
-# time_series = cd_model.simulate_ts(k_init=3, ts_length=100, plot=True)
-# print(time_series)
 
 # Plot the relation of steady state consumption and savings rate
-# s_grid = np.linspace(0.1, 0.9, 1000)
-# consumption = cd_model.plot_consumption_savings(s_grid)
+sgrid = np.linspace(0.1, 0.9, 100)
+plot_consumption_savings(cd_model, sgrid)
+# Plot the 45-degree line and the capital accumulation curve
+kgrid = np.linspace(0, 3, 1000)
+plot45(cd_model, kgrid)
+# Plot the evolution of capital per capita
+time_series = cd_model.simulate_ts(k_init=3, ts_length=12)
+plot_time_series(cd_model, time_series)
 
 # CES production model
-ces_parameters = {"s": 0.3, "delta": 0.4, "k0": 0.25, "production": "CES", "A": 2.0, "alpha": 0.3, "rho": 0.5}
-ces_model = Solow(**ces_parameters)
-ces_model.plot45(np.linspace(0, 3, 1000), ces_model.steady_state())
-ces_model.simulate_ts(k_init=3, ts_length=100, plot=True)
-s_grid = np.linspace(0.1, 0.9, 1000)
-ces_model.plot_consumption_savings(s_grid)
+ces_parameters = {
+    "A": 2.0,
+    "alpha": 0.5,
+    "rho": 0.5,
+}
+ces_function = CES(**ces_parameters)
+ces_model = Solow(savings=0.3, delta=0.4, k_0=0.25, production=ces_function)
+
+# Plot the relation of steady state consumption and savings rate
+plot_consumption_savings(ces_model, sgrid)
+# Plot the 45-degree line and the capital accumulation curve
+plot45(ces_model, kgrid)
+# Plot the time series
+time_series = ces_model.simulate_ts(k_init=3, ts_length=12)
+plot_time_series(ces_model, time_series)
--- solow/solow.py
+++ solow/solow.py
@@ -1,40 +1,27 @@
+"""Implementation of the Solow growth model"""
+
+from abc import ABC, abstractmethod
+
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy import optimize
+
+# from dataclasses import dataclass
 
 
 class Solow:
-    def __init__(self, s, delta, k0, production="Cobb-Douglas", **kwargs):
-        self.s, self.delta, self.k0, self.production = s, delta, k0, production
+    """Solow growth model"""
 
-        # Cobb-Douglas attributes
-        if production == "Cobb-Douglas":
-            self.A, self.alpha = kwargs.get('A'), kwargs.get('alpha')
-        # CES attributes
-        elif production == "CES":
-            self.A, self.rho, self.alpha = kwargs.get('A'), kwargs.get('rho'), kwargs.get('alpha')
-        else:
-            raise ValueError("Production function must be either 'Cobb-Douglas' or 'CES'")
+    def __init__(self, savings, delta, k_0, production):
+        self.savings = savings
+        self.delta = delta
+        self.k_0 = k_0
+        self.production = production
 
     def __repr__(self) -> str:
-        return f"Solow model with parameters:\n s:{self.s}\n delta:{self.delta}\n k0:{self.k0} \n Production:{self.production}\n"
+        return f"Solow model with parameters:\n savings:{self.savings}\n delta:{self.delta}\n k_0:{self.k_0} \n production:{self.production}\n"
 
-    def f(self, k):
-        """Production function as a function of capital per capita
-
-        :param k: effective capital per capita
-        :type k: float numpy array
-        :return: production level
-        :rtype: float numpy array
-        """
-        k = np.asarray(k)
-        if self.production == "Cobb-Douglas":
-            return self.A * k**self.alpha
-        elif self.production == "CES":
-            return self.A * (self.alpha * k**self.rho + (1 - self.alpha))**(1 / self.rho)
-
-    def k_1(self, k):
-        """Capital accumulation
+    def k_update(self, k):
+        """Capital accumulation rule
 
         :param k: effective capital per capita
         :type k: float numpy array
@@ -43,7 +30,7 @@ class Solow:
         """
 
         k = np.asarray(k)
-        return (1 - self.delta) * k + self.s * self.f(k)
+        return (1 - self.delta) * k + self.savings * self.production.function(k)
 
     def steady_state(self):
         """Steady state of the model
@@ -51,10 +38,11 @@ class Solow:
         :return: steady state capital per capita
         :rtype: float
         """
+
         # Objective function to find the root
         def obj(k):
-            return k - self.k_1(k)
-        
+            return k - self.k_update(k)
+
         try:
             # Find the root of the objective function
             return optimize.root_scalar(obj, bracket=[0.01, 100]).root
@@ -69,94 +57,112 @@ class Solow:
         :return: consumption value
         :rtype: float
         """
-        return (1 - self.s) * self.f(self.steady_state())
-
-    def plot45(self, kgrid, kstar):
-        """Plot the 45-degree line and the capital accumulation curve
-
-        :param kgrid: grid of capital per capita values
-        :type kgrid: float numpy array
-        :param kstar: steady state capital per capita
-        :type kstar: float
-        """
-        fig, ax = plt.subplots(figsize=(8, 6))
-        ax.plot(kgrid, self.k_1(kgrid), "b-", label="$k_{t+1}$")
-        ax.plot(kgrid, kgrid, "k--", label="$45$")
-        # plot a star at the steady state
-        ax.plot(kstar, kstar, "*", markersize=10.0, color="green")
-        # annotate the value of kstar
-        ax.annotate(
-            f"$k^*=${kstar:.2f}",
-            xy=(kstar, kstar),
-            xytext=(kstar + 0.1, kstar - 0.1),
-            fontsize=12,
-            color="green",
+        return (1 - self.savings) * self.production.function(
+            self.steady_state()
         )
-        ax.set_title("Capital accumulation")
-        ax.set_xlabel("$k_t$")
-        ax.set_ylabel("$k_{t+1}$")
-        ax.legend()
-        plt.show()
 
-    def simulate_ts(self, k_init, ts_length, plot=True):
-
+    def simulate_ts(self, k_init, ts_length):
         ts = np.zeros(ts_length)
         ts[0] = k_init
         for t in range(1, ts_length):
-            ts[t] = self.k_1(ts[t - 1])
-
-        if plot:
-            fig, ax = plt.subplots(figsize=(8, 6))
-            ax.plot(ts, "b-", label="$k_{t+1}$")
-            ax.set_xlabel("$t$")
-            ax.set_ylabel("$k_{t+1}$")
-            ax.set_title("Time series for capital per capita")
-            ax.legend()
-            plt.show()
+            ts[t] = self.k_update(ts[t - 1])
 
         return ts
 
-    def plot_consumption_savings(self, s, plot=True):
+    def capital_savings(self, sgrid):
+        """Steady state capital per capita for different saving rates
+
+        :param sgrid: Grid of saving rates
+        :type sgrid: numpy array
+        :return: Steady state capital per capita values
+        :rtype: numpy array
+        """
+        sgrid = np.asarray(sgrid)
+        kgrid = np.empty(len(sgrid))
+
+        # Objective function to find the root
+        def obj(x, s):
+            return x - ((1 - self.delta) * x + s * self.production.function(x))
+
+        for i, s in enumerate(sgrid):
+            kgrid[i] = optimize.root_scalar(
+                obj, args=(s), bracket=[0.001, 100]
+            ).root
+
+        return kgrid
+        # try:
+        #     # Find the root of the objective function
+        #     return optimize.root_scalar(obj, bracket=[0.01, 100]).root
+        # except:
+        #     print("No steady state found")
+        #     return None
+
+    def consumption_savings(self, sgrid):
         """Consumption per capita in the steady state
 
         :param s: Exogenous saving rates
         :type s: numpy array
-        :param plot: Show the plot, defaults to True
-        :type plot: bool, optional
         :return: Consumption per capita values
         :rtype: numpy array
         """
-        s = np.asarray(s)
+        sgrid = np.asarray(sgrid)
         # Compute steady state capital per capita for the given saving rate
-        kstar = ((s * self.A) / (self.delta)) ** (1 / (1 - self.alpha))
-        consumption = (1 - s) * self.f(kstar)
+        kstar = self.capital_savings(sgrid)
+        consumption = (1 - sgrid) * self.production.function(kstar)
         # Golden rule saving rate
         golden_idx = np.argmax(consumption)
-        golden_savings = s[golden_idx]
+        golden_savings = sgrid[golden_idx]
         golden_consumption = consumption[golden_idx]
 
-        fig, ax = plt.subplots(figsize=(8, 6))
-        ax.plot(s, consumption, "b-", label="$k^*$")
-        ax.plot(
-            golden_savings,
-            golden_consumption,
-            "*",
-            markersize=10.0,
-            color="green",
-        )
-        # Annotate the golden rule
-        ax.annotate(
-            f"Golden rule: $s_{{gr}}=${golden_savings:.2f}",
-            xy=(golden_savings, golden_consumption),
-            xytext=(golden_savings - 0.1, golden_consumption - 0.4),
-            fontsize=12,
-            color="green",
-        )
-        ax.set_xlabel("Saving rate")
-        ax.set_ylabel("Steady state consumption")
-        ax.legend()
+        return {
+            "consumption": consumption,
+            "consumption_gr": golden_consumption,
+            "savings_gr": golden_savings,
+        }
 
-        if plot:
-            plt.show()
 
-        return consumption
+class Production(ABC):
+    """Base class for production functions."""
+
+    @abstractmethod
+    def function(self, k) -> float:
+        """Abstract representation of the production function"""
+
+
+class CobbDouglas(Production):
+    """Cobb-Douglas production function"""
+
+    def __init__(self, A, alpha):
+        self.A = A
+        self.alpha = alpha
+
+    def __repr__(self) -> str:
+        return "Cobb-Douglas"
+
+    def function(self, k):
+        """Production function as a function of capital per capita
+        :param k: effective capital per capita
+        :type k: float numpy array
+        :return: production level
+        :rtype: float numpy array
+        """
+        k = np.asarray(k)
+        return self.A * k**self.alpha
+
+
+class CES(Production):
+    """CES production function"""
+
+    def __init__(self, A, alpha, rho):
+        self.A = A
+        self.alpha = alpha
+        self.rho = rho
+
+    def __repr__(self) -> str:
+        return "CES"
+
+    def function(self, k):
+        k = np.asarray(k)
+        return self.A * (self.alpha * k**self.rho + (1 - self.alpha)) ** (
+            1 / self.rho
+        )
